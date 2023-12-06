a27158b02f44c68d81cde18c3a6c2e790b56a5f6
mohamad alshawa <mohamad.alchawwa+github@gmail.com>
2023-12-06 22:13:40 +0200

    .

---
+++ backend/models/WorkoutModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const workoutSchema = new Schema({
+    title :{
+        type: String,
+        required: true
+    },
+
+
+    reps :{
+        type: Number,
+        required: true
+    },
+
+
+    load:{
+        type: Number,
+        required: true
+    }
+ 
+
+    },{timestamps: true})// this will automatically create a timestamp for each workout
+
+    module.exports= mongoose.model('Workout', workoutSchema); 
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const Workout = require('../models/WorkoutModel');
 
 const router = express.Router();
 
@@ -16,9 +16,16 @@ router.get('/:id', (req, res) => {
 
 
 //POST a workout
-router.post('/', (req, res) => {
-    res.json({message: 'POST a workout'});
-})
+router.post('/', async (req, res) => {
+    const {title, load, reps} = req.body
+    
+    try {
+      const workout = await Workout.create({title, load, reps})
+      res.status(200).json(workout)
+    } catch (error) {
+      res.status(400).json({error: error.message})
+    }
+  })
 
 //DELETE a workout
 router.delete('/:id', (req, res) => {
