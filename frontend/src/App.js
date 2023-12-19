a5bdbec1bae75543f39f0992fde12811f51df6dd
mohamad alshawa <mohamad.alchawwa+github@gmail.com>
2023-12-19 21:38:31 +0200

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
 import { BrowserRouter ,Routes, Route } from 'react-router-dom';
 
 
-//pages and components
+//pages 
 import Home from './pages/Home';
 
+
+//components
+import NavBar from './components/NavBar';
 
 function App() {
   return (
    <div className="App">
     <BrowserRouter>
+    {/* must be in the BrowserRouter tags */}
+    <NavBar /> 
      <div className='pages'>
       <Routes>
         <Route
---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function NavBar() {
+  return (
+    <header>
+    <div className='container'>
+      <Link to='/' >
+        <h1>Workout Tracker</h1>
+        </Link>
+    </div>
+    </header>
+  )
+}
