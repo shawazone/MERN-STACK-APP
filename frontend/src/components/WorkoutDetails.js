ea0b880156999fdc08189c128439626a0da63303
mohamad alshawa <mohamad.alchawwa+github@gmail.com>
2023-12-24 21:28:45 +0200

    ,

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,12 +1,31 @@
 import React from 'react'
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+
+
 
 const WorkoutDetails = ({workout}) => {
+  const {dispatch} = useWorkoutsContext() 
+  
+  const handleClick = async() => {
+   const response = await fetch('/api/workouts/'+workout._id, {
+    method : 'DELETE',
+   })  
+   const json = await response.json()
+  
+    if (response.ok) {
+      console.log(json)
+      dispatch({type:'DELETE_WORKOUT', payload:json})
+    }
+  }
+
+
   return (
     <div className='workout-details'>
         <h4>{workout.title}</h4>
         <p><strong>Load (kg):</strong>{workout.load}</p>
         <p><strong>Reps:</strong>{workout.reps}</p>
         <p>{workout.ceated}</p>
+        <span onClick={handleClick}>delete</span>
     </div>
   )
 }
