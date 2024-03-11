5ef2b9c692543f2b9423db67629995d60cd4e7fd
mohamad alshawa <mohamad.alchawwa+github@gmail.com>
2024-03-11 02:29:35 +0200

    .

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,15 @@
 const User = require('../models/userModel');
-const jwt = require('jsonwetoken');
+const jwt = require('jsonwebtoken');
 
 const createToken = (_id) => {
-    jwt.sign({_id:_id},process.env.JWT_SECRET,);// do not put sensetive data here like password or email or anything that can be used to identify the user
+   return  jwt.sign({_id:_id},process.env.JWT_SECRET,{expiresIn:'1d'});// do not put sensetive data here like password or email or anything that can be used to identify the user
+// the expiresIn is an option 
 }
 //login user
 const loginUser = (req, res) => {
 res.json({message: 'login user'});
 };
+//uwu
 
 
 //signup user
@@ -16,8 +18,11 @@ const signupUser = async (req, res) => {
 
     try{
         const user = await User.signup(email, password);
+
+        // create token 
+        const token = createToken(user._id);
    
-        res.status(201).json({email, user});
+        res.status(201).json({email, token});
     }
     catch(err){
         res.status(400).json({message: err.message});
