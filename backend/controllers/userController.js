b007df4d2eb328270b11a6a9e4f270f8e7973ea1
mohamad alshawa <mohamad.alchawwa+github@gmail.com>
2024-03-14 12:34:53 +0200

    working on auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,32 +5,43 @@ const createToken = (_id) => {
    return  jwt.sign({_id:_id},process.env.JWT_SECRET,{expiresIn:'1d'});// do not put sensetive data here like password or email or anything that can be used to identify the user
 // the expiresIn is an option 
 }
-//login user
-const loginUser = (req, res) => {
-res.json({message: 'login user'});
-};
+// login a user
+const loginUser = async (req, res) => {
+  const {email, password} = req.body
+
+  try {
+    const user = await User.login(email, password)
+
+    // create a token
+    const token = createToken(user._id)
+
+    res.status(200).json({email, token})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+}
 //uwu
+
 
 
 //signup user
 const signupUser = async (req, res) => {
-    const {email, password} = req.body;
+  const {email, password} = req.body;
 
-    try{
-        const user = await User.signup(email, password);
+  try{
+      const user = await User.signup(email, password);
 
-        // create token 
-        const token = createToken(user._id);
-   
-        res.status(201).json({email, token});
-    }
-    catch(err){
-        res.status(400).json({message: err.message});
-    }
+      // create token 
+      const token = createToken(user._id);
+ 
+      res.status(201).json({email, token});
+  }
+  catch(err){
+      res.status(400).json({message: err.message});
+  }
 
 // res.json({message: 'signup user'});
 };
-
 
 module.exports = {
 loginUser,
